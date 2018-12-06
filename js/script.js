3f3b3df76fdf11ccc2af2413fd3a1fbe666fa890
wowxenia <40713947+wowxenia@users.noreply.github.com>
2018-12-06 08:29:07 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $(function() {
+    $(".preload").fadeOut(1000, function() {
+        $(".content").fadeIn(500); 
+        $(".WdgMoyklassModalItem-dialog").fadeIn(500); 
+    $(function(){
+		$('a[href^="#"]').click(function(){
+			var target = $(this).attr('href');
+			$('html, body').animate({scrollTop: $(target).offset().top}, 1000);
+			return false; 
+		}); 
+});
+});
+});
+});
+
