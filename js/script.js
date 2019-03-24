2d3a88858b376115093837051dea5954ab002a5c
wowxenia <40713947+wowxenia@users.noreply.github.com>
2019-03-24 23:52:29 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,16 +1,38 @@
-$(document).ready(function() {
-    $(function() {
-    $(".preload").fadeOut(1000, function() {
-        $(".content").fadeIn(500); 
-        $(".WdgMoyklassModalItem-dialog").fadeIn(500); 
-    $(function(){
-		$('a[href^="#"]').click(function(){
-			var target = $(this).attr('href');
-			$('html, body').animate({scrollTop: $(target).offset().top}, 1000);
-			return false; 
-		}); 
+ $(document).ready(function(){
+	$("#menu").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		let id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
 });
-});
-});
+
+
+// modal 
+
+let modal = document.querySelector("#modal"),
+	modalOverlay = document.querySelector("#modal-overlay"),
+	closeButton = document.querySelector("#close-buy");
+	openButton = document.querySelectorAll(".buy");
+
+if (openButton) {
+  openButton.forEach(function(el){
+     el.addEventListener('click', function () {
+        modal.classList.toggle("closed");
+		modalOverlay.classList.toggle("closed");
+     });
+  });
+}
+
+closeButton.addEventListener("click", function() {
+	modal.classList.toggle("closed");
+	modalOverlay.classList.toggle("closed");
 });
 
