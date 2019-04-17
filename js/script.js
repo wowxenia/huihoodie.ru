b78a45e3bb8464f8fa72d89e2f78395237ffd49a
wowxenia <40713947+wowxenia@users.noreply.github.com>
2019-04-17 18:30:39 +0300

    Add Age Verification

--- js/script.js
+++ js/script.js
@@ -36,3 +36,25 @@ closeButton.addEventListener("click", function() {
 	modalOverlay.classList.toggle("closed");
 });
 
+
+
+// age verification
+
+$(document).ready(function(){
+  // put the popup at the start of the body
+  $('#ageWrapper').prependTo($('body'));
+
+  // check if the age has already been verified
+  if (($.cookie('age')) !== 'true') { $('#ageWrapper').addClass('ageConfirmed'); }
+
+  // if the "yes" button is clicked, add a cookie and hide the popup
+  $('#ageOkay').click(function() {
+    $.cookie('age', 'true', { expires: 1, path: '/' });
+    $('#ageWrapper').removeClass('ageUnknown');
+  });
+
+  // if the "no" button is clicked, take the user elsewhere
+  $('#ageBad').click(function() {
+    window.location.href='https://findtheinvisiblecow.com/';
+  });
+});
