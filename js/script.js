0e7f2546f034edef51546c4717cfdd73884eaff5
wowxenia <40713947+wowxenia@users.noreply.github.com>
2019-04-19 22:40:03 +0300

    feat: add shaked cat

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
  $(document).ready(function(){
 	$("#menu").on("click","a", function (event) {
 		//отменяем стандартную обработку нажатия по ссылке
-		event.preventDefault();
+		// event.preventDefault();
 
 		//забираем идентификатор бока с атрибута href
 		let id  = $(this).attr('href'),
 
 		//узнаем высоту от начала страницы до блока на который ссылается якорь
-			top = $(id).offset().top;
+		top = $(id).offset().top;
 		
 		//анимируем переход на расстояние - top за 1500 мс
 		$('body,html').animate({scrollTop: top}, 1500);
